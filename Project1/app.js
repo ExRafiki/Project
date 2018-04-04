b30fed1fd5e4a7f24e3b48efeb04025376f13925
ExRafiki <shiizzlle00@gmail.com>
2018-04-04 14:07:17 +0100

    update

--- Project1/app.js
+++ Project1/app.js
@@ -2,13 +2,22 @@ var cells = [];
 
 $(function(){
 
-  const shipsLengths = [5,3,2,1];
+  const shipsLengths = [5,4,2,1];
   const generatedBoard = new Board(8,8);
   var score = 0;
   let x = null;
   let y = null;
   const DEV = true;
 
+  function Board(width, height){
+    for(let i = 0; i < width; i++){
+      cells.push([]);
+      for(let j = 0; j < height; j++){
+        cells[i].push(0);
+        $(`<div class="grid-item" id="cell_${i}_${j}" data-ship='${null}' data-x='${i}' data-y='${j}'>`).appendTo('.grid-container');
+      }
+    }
+  }
   function findPosition(shipLength, horizontal){
     let currentTestPositions = [];
     do {
@@ -29,16 +38,6 @@ $(function(){
 
     return [x, y];
   }
-
-  function Board(width, height){
-    for(let i = 0; i < width; i++){
-      cells.push([]);
-      for(let j = 0; j < height; j++){
-        cells[i].push(0);
-        $(`<div class="grid-item" id="cell_${i}_${j}" data-ship='${null}' data-x='${i}' data-y='${j}'>`).appendTo('.grid-container');
-      }
-    }
-  }
   function makeShip(shipLength){
     var randomNumber = Math.floor(Math.random() * 2);
     let position = null;
@@ -53,30 +52,26 @@ $(function(){
     }else{
       position = findPosition(shipLength, false);
     }
-
     for(let i = 0; i< shipLength ;i++){
       cells[position[0]][position[1]+i] = 1;
-      // console.log(`#cell_${x}_${y+i}`);
       if (way === 'H') {
         $(`#cell_${x}_${y+i}`).data('ship', 'true');
-        if(DEV)
-          $(`#cell_${x}_${y+i}`).css('background', 'red');
+        // if(DEV)
+        //   $(`#cell_${x}_${y+i}`).css('background', 'red');
       } else{
         $(`#cell_${x+i}_${y}`).data('ship', 'true');
-        if(DEV)
-          $(`#cell_${x+i}_${y}`).css('background', 'red');
+        // if(DEV)
+        //   $(`#cell_${x+i}_${y}`).css('background', 'red');
       }
     }
-
-
   }
-
   $(shipsLengths).each(function(i, shipsLengths){
     makeShip(shipsLengths);
   });
 
   $('.grid-item').on('click', function() {
     if($(this).data('ship') === 'true') {
+      // if($(this).css('background', 'red'))
       $(this).css('background', 'red');
       score += 5 ;
       $('p.Score').text('Score : ').append(score);
@@ -85,6 +80,3 @@ $(function(){
     }
   });
 });
-// add the image to appear when the div is a hit
-// add the correct hit to the score board
-// high score added and saved locally
