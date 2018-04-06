7e1a4def225eb0b6fc068674f736e64be279185c
ExRafiki <shiizzlle00@gmail.com>
2018-04-06 12:23:36 +0100

    readme done- game code polished

--- Project1/app.js
+++ Project1/app.js
@@ -1,25 +1,18 @@
 var cells = [];
-//
-// window.onload = function () {
-//   document.getElementsByTagName('.close').onclick = function () {
-//     document.getElementById('myModal').style.display = 'none';
-//   };
-// };
-
 $(function(){
-//---------------VARIABLES-----------------------------------------------------
+//------------------VARIABLES---------------------------------------------------
   const shipsLengths = [3,3,3,3];
   var score = 0;
-  // var internalScore = 0;
   let x = null;
   let y = null;
   let clickCount = 0;
   const gg = (adding, currentValue) => adding + currentValue;
-  const clickCountMax = 25;
-  const DEV = true;
+  const clickCountMax = 40;
+  //----------------DEV TRIGGER-------------------------------------------------
+  // const DEV = true;
   //----------------BOARD LOAD--------------------------------------------------
   Board(8,8);
-  //---------------BOARD FUNCTION-----------------------------------------------
+  //----------------BOARD FUNCTION----------------------------------------------
   function Board(width, height){
     if(cells.length > 0){
       cells = [];
@@ -28,11 +21,12 @@ $(function(){
       cells.push([]);
       for(let j = 0; j < height; j++){
         cells[i].push(0);
-        $(`<div class="grid-item" id="cell_${i}_${j}" data-ship='${null}' data-x='${i}' data-y='${j}'>`).appendTo('.grid-container');
+        $(`<div class="grid-item" id="cell_${i}_${j}" data-ship='${null}'
+        data-x='${i}' data-y='${j}'>`).appendTo('.grid-container');
       }
     }
   }
-  //---------------HORIZ/VERTIC FINDER------------------------------------------
+  //----------------HORIZ/VERTIC FINDER-----------------------------------------
   function findPosition(shipLength, horizontal){
     let currentTestPositions = [];
     do {
@@ -44,7 +38,7 @@ $(function(){
         x = Math.floor(Math.random() * (cells.length - shipLength + 1));
         y = Math.floor(Math.random() * (cells.length));
       }
-      console.log(`finding position for ship with ${shipLength}(${horizontal}) starting from ${x}, ${y}`);
+      console.log(`finding pos for ship with ${shipLength}from ${x}, ${y}`);
       for(let i = 0; i< shipLength ;i++){
         const position = horizontal ? cells[x][y+i] : cells[x+i][y];
         currentTestPositions.push(position);
@@ -53,14 +47,14 @@ $(function(){
     } while (!currentTestPositions.every((cell) => cell === 0));
     return [x, y];
   }
-  //------------ POSITION MAKER -----------------------------------------------
+  //----------------POSITION MAKER ---------------------------------------------
   function makeShip(shipLength){
     var randomNumber = Math.floor(Math.random() * 2);
     let position = null;
     const way = randomNumber === 1 ? 'H': 'V';
     if (way === 'H') {
-      position = findPosition(shipLength, true); // 2nd argument is for Horizontal or Vertical, true === horizontal
-    }else{
+      position = findPosition(shipLength, true); // 2nd argument is for Hor
+    }else{//or Vertical, true === horizontal
       position = findPosition(shipLength, false);
     }
     for(let i = 0; i< shipLength ;i++){
@@ -68,46 +62,44 @@ $(function(){
       if (way === 'H') {
         cells[position[0]][position[1]+i] = 1;
         $(`#cell_${x}_${y+i}`).data('ship', 'true');
-        if(DEV)
-          $(`#cell_${x}_${y+i}`).css('background', 'red');
-
+        // if(DEV)
+        //   $(`#cell_${x}_${y+i}`).css('background', 'red');
       } else{
         cells[position[0]+i][position[1]] = 1;
         $(`#cell_${x+i}_${y}`).data('ship', 'true');
-        if(DEV)
-          $(`#cell_${x+i}_${y}`).css('background', 'red');
+        // if(DEV)
+        //   $(`#cell_${x+i}_${y}`).css('background', 'red');
       }
     }
   }
-  //------------------CALL TO MAKE THE SHIPS-----------------------------------
+  //-----------------CALL TO MAKE THE SHIPS-------------------------------------
   $(shipsLengths).each(function(i, shipLength){
     makeShip(shipLength);
   });
-  //----//-------//------CLICK LISTENER---//------//----------//--------//-----
+  //----//-------//--CLICK LISTENER---//------//----------//--------//----------
   $('.grid-container').on('click','.grid-item',triggerAll);
-  //--------------------ATTEMPT------------------------------------------------
+  //-----------------CLICK FUNCTION GROUP---------------------------------------
+  let clicked = false;
   function triggerAll(){
     checkBox.call(this);
     clickCounter();
     reset();
   }
-  //--------------------ATTEMP REFACTOR/CLEANER FUNCTIONS----------------------
+  //-----------------CLEANER FUNCTIONS------------------------------------------
   function checkBox(){
     if($(this).data('ship') === 'true') {
-      console.log('checkBox i am here');
       $(this).css('background', 'yellow');
       score += 1 ;
+      clicked = true;
       $('p.Score').text('Score : ').append(score);
     } else {
       $(this).css('background', 'black');
-      // score -= 1;
     }
   }
   function clickCounter() {
     clickCount++;
-    console.log('clickCounter clicked!!!');
     if(clickCount >= clickCountMax){
-      alert('YOU really are bad');
+      alert('YOU really are terrible at this');
       clickCount= 0;
       $( '.grid-container').empty();
       Board(8,8);
@@ -117,10 +109,11 @@ $(function(){
     }
   }
   function reset(){
-    if ((score % shipsLengths.reduce(gg)) === 0){
-      console.log('reset is here');
+    if ((score % shipsLengths.reduce(gg)) === 0 && clicked ){
+      alert('Good Job!Now try the biggest score possible');
       $( '.grid-container').empty();
       Board(8,8);
+      clicked = false;
       $(shipsLengths).each(function(i, shipLength){
         makeShip(shipLength);
         clickCount = 0;
@@ -128,3 +121,4 @@ $(function(){
     }
   }
 });
+//------------------THE END-----------------------------------------------------
---
+++ Project1/readme.md
@@ -0,0 +1,83 @@
+<a href="https://imgur.com/OBO10UL"><img src="https://i.imgur.com/OBO10UL.png" title="source: imgur.com" /></a>
+
+<span style="color:red">##GA PROJECT NUMBER 1:</span>
+
+###Goal:
+
+Make a fun game that can be played more then once (mainly as a time killer).
+To create a functional game that has a clean code as DRY as i can manage.
+Focusing on a scalable game so it can be upgraded as the course goes on.
+User friendly and interactive
+
+###Languages used:
+
+* HTML5
+* Vanilla JS / Jquery
+* CSS
+
+###Released on
+
+* Github
+
+##Game - BATTLEFIND
+
+The point of the game is to find the hidden ships in the grid.Limited clicks make
+it so that a high score can only be achieved if correct guesses are made consistently.
+
+###Visit game
+
+ [Github](link).
+
+###ScreenShot
+
+<a href="https://imgur.com/FlBR6Vm"><img src="https://i.imgur.com/FlBR6Vm.png" title="source: imgur.com" /></a>
+
+<a href="https://imgur.com/Dx2kbXo"><img src="https://i.imgur.com/Dx2kbXo.png" title="source: imgur.com" /></a>
+
+##Process
+
+###Research
+
+I started with searching all basic games available on Appstore or small game websites.
+While searching , i found out that battleship has been done over and over , but following the traditional
+concept which is player 1 and player 2 fighting each other.I could not find a game
+that had the RNG(RandomNumberGeneration) factor to it which adds re-playability.
+
+###Development
+
+* 1 -Using other battleship game structures i set up a general javascript structure that
+game a certain logic to the concept.
+* 2 -Working slowing on the game logic i set up ship generation then added the horizontal/
+vertical.
+* 3 -Spend 2 days figuring the array positioning as well as making it "true" RNG so
+no one pattern is the same
+
+###Lessons learned:
+
+*  Focus on core structures and more my way up the problem rather then try to fix
+it all at once.
+*  Consistent use of console log to locate the issue as well as see how the FUNCTIONS
+are running.
+
+###Challenges
+
+*  Original functions layout as well as the use of 2D array using random generation.
+*  Google Chrome Dev tools - not displaying the changing in value of the grid-item's data-ship,
+making me waste a lot of time trying to change the data-attribute.
+*  The refactoring so the game can be played ''forever''.
+*  Time management , as i didn't have time to focus on aesthetics.
+
+###Goals achieved
+
+- [x]Make a fun game that can be played more then once (mainly as a time killer).
+- [x]To create a functional game that has a clean code as DRY as i can manage.
+- [x]Focusing on a scalable game so it can be upgraded as the course goes on.
+- [ ] <span style="color:red">User friendly and interactive text/animation</span>
+
+###Future Features
+
+* The future idea that i would like to be implement would be :instructions, fade
+ in , single player or online, leaderboard and finally animation to make it more
+ interactive.
+
+<a href="https://imgur.com/BaOik36"><img src="https://i.imgur.com/BaOik36.png?1" title="source: imgur.com" /></a>
